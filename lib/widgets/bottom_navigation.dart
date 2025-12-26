d61dc8c34e8f8d70383d271ee685869f5c6dfc37
Emka-linux <kubalyz@proton.me>
2025-12-26 21:07:28 +0100

    design

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -14,45 +14,50 @@ class BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
+      height: 85,
       decoration: BoxDecoration(
-        color: Colors.grey[200],
-        border: Border(
-          top: BorderSide(color: Colors.grey[400]!, width: 2),
+        color: const Color(0xFFFFF5F7), // Soft cream/beige background
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+        border: Border.all(
+          color: const Color(0xFFB8A8D8), // Soft purple border
+          width: 3,
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _NavButton(
-            key: const Key('nav_button_0'),
-            icon: MyFlutterApp.house,
-            color: const Color.fromARGB(0, 219, 161, 161),
-            isSelected: selectedIndex == 0,
-            onTap: () => onItemTapped(0),
-          ),
-          _NavButton(
-            key: const Key('nav_button_1'),
-            icon: MyFlutterApp.shop,
-            color: const Color.fromARGB(0, 127, 179, 60),
-            isSelected: selectedIndex == 1,
-            onTap: () => onItemTapped(1),
-          ),
-          _NavButton(
-            key: const Key('nav_button_3'),
-            icon: MyFlutterApp.task,
-            color: const Color.fromARGB(0, 105, 154, 226),
-            isSelected: selectedIndex == 3,
-            onTap: () => onItemTapped(3),
-          ),
-          _NavButton(
-            key: const Key('nav_button_4'),
-            icon: MyFlutterApp.man,
-            color: const Color.fromARGB(0, 105, 154, 226),
-            isSelected: selectedIndex == 4,
-            onTap: () => onItemTapped(4),
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _NavButton(
+              key: const Key('nav_button_0'),
+              icon: MyFlutterApp.house,
+              color: const Color(0xFFE89BAC), // Soft pink
+              isSelected: selectedIndex == 0,
+              onTap: () => onItemTapped(0),
+            ),
+            _NavButton(
+              key: const Key('nav_button_1'),
+              icon: MyFlutterApp.shop,
+              color: const Color(0xFFB8D98E), // Soft green
+              isSelected: selectedIndex == 1,
+              onTap: () => onItemTapped(1),
+            ),
+            _NavButton(
+              key: const Key('nav_button_3'),
+              icon: MyFlutterApp.task,
+              color: const Color.fromARGB(255, 157, 204, 224), // clue
+              isSelected: selectedIndex == 3,
+              onTap: () => onItemTapped(3),
+            ),
+            _NavButton(
+              key: const Key('nav_button_4'),
+              icon: MyFlutterApp.man,
+              color: const Color.fromARGB(255, 239, 187, 199), // Soft pink
+              isSelected: selectedIndex == 4,
+              onTap: () => onItemTapped(4),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -77,27 +82,32 @@ class _NavButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 60,
-        height: 60,
+        width: 56,
+        height: 56,
         decoration: BoxDecoration(
-          color: isSelected ? color.withOpacity(0.2) : Colors.transparent,
+          color: isSelected ? color.withOpacity(0.25) : Colors.white.withOpacity(0.5),
           shape: BoxShape.circle,
           border: Border.all(
-            color: isSelected ? color : Colors.grey[400]!,
-            width: isSelected ? 3 : 1,
+            color: color,
+            width: 3,
+          ),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: color.withOpacity(0.3),
+                    blurRadius: 8,
+                    offset: const Offset(0, 3),
+                  ),
+                ]
+              : [],
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 32,
+            height: 32,
+            child: icon,
           ),
         ),
-        child: Container(
-                  child: ColorFiltered(
-                            colorFilter: ColorFilter.mode(
-                              color, // Color and opacity
-                              BlendMode.srcATop, // Blend mode
-                            ),
-                            child: icon, // Replace with your image asset
-                          ),            
-                          width: 32,
-                          height: 32,
-                ),
       ),
     );
   }
