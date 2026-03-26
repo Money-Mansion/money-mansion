24e025cd7455085c9f4453a9d8d8ee14d20dd171
Šimon Š <simon.skombar@gmail.com>
2026-03-26 20:16:29 +0100

    nove itemy

---
+++ lib/config/items/carpets.dart
@@ -0,0 +1,125 @@
+import '../../models/item.dart';
+
+/// Carpet items - floor coverings and rugs.
+final List<Item> carpetItems = [
+  // Add carpet items here
+  Item(
+    id: 'koberec_skibidi',
+    name: 'Skibidi koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_skibidi.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_real',
+    name: 'Realistický koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_real.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_oblacik',
+    name: 'Koberec s obláčikom',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_oblacik.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_modry',
+    name: 'Modrý koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_modry.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_magia',
+    name: 'Magický koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_magia.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_macka',
+    name: 'Mačací koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_macka.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_macka_siva',
+    name: 'Sivý mačací koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_macka_siva.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_hnedy_ciary',
+    name: 'Hnedý koberec s čiarami',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_hnedy_ciary.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_duha',
+    name: 'Duhový koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_duha.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_cute',
+    name: 'Zlatý koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_cute.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_babka',
+    name: 'Babkin koberec',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_babka.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+
+  Item(
+    id: 'koberec_4_znaky',
+    name: 'Koberec so 4 znakmi',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/koberec_4_znaky.png',
+    cost: 0,
+    hitboxId: 'koberec',
+    scale: 0.11,
+  ),
+];
--- lib/config/items/furniture.dart
+++ lib/config/items/furniture.dart
@@ -2,6 +2,79 @@ import '../../models/item.dart';
 
 /// Furniture items - sofas, beds, cabinets, wardrobes
 final List<Item> furnitureItems = [
+  Item(
+    id: 'polica_kniznica_cierna',
+    name: 'Čierna knižnica',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/polica_kniznica_cierna.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.14,
+  ),
+
+  // Basic Shelves
+  Item(
+    id: 'polica',
+    name: 'Basic polica',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/polica.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.125,
+  ),
+
+  Item(
+    id: 'polica_dizajnová',
+    name: 'Dizajnová polica',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/polica_dizajnová.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.13,
+  ),
+
+  // Cabinet/Storage
+  Item(
+    id: 'skriňa_rohová',
+    name: 'Rohová skriňa',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/skriňa_rohová.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.17,
+  ),
+
+  Item(
+    id: 'skriňa_poličky',
+    name: 'Skriňa s poličkami',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/skriňa_poličky.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.15,
+  ),
+
+  Item(
+    id: 'skriňa_nízka',
+    name: 'Nízká skriňa',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/skriňa_nízka.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.125,
+  ),
+
+  // Beds (decorative)
+  Item(
+    id: 'posteľ_zámok',
+    name: 'Posteľ na zámku',
+    type: ItemType.decoration,
+    texture: 'assets/images/items/posteľ_zámok.png',
+    cost: 0,
+    hitboxId: null,
+    scale: 0.17,
+  ),
+  
   // Sofas/Couches
   Item(
     id: 'gauc_zeleny',
