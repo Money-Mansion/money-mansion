6de831bd38ecbaaacc93be351d8e23e3579e1daf
Emka-linux <kubalyz@proton.me>
2026-04-14 20:31:46 +0200

    farby daktore

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -137,7 +137,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         return Scaffold(
           appBar: AppBar(
             title: Text(l10n.translate('settings')),
-            backgroundColor: Colors.orange[500],
+            backgroundColor: const Color.fromARGB(255, 149, 117, 205),
             elevation: 0,
             automaticallyImplyLeading: false,
             leading: IconButton(
@@ -178,7 +178,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                                   _showSnack(l10n.translate('tutorialRestart'));
                                 },
                                 style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.deepOrange,
+                                  backgroundColor: const Color.fromARGB(255, 103, 58, 183),
                                   foregroundColor: Colors.white,
                                 ),
                                 child: Text(l10n.translate('tutorialRestart')),
@@ -221,7 +221,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                                 decoration: BoxDecoration(
                                   border: Border.all(
                                     color: isSelected
-                                        ? Colors.orange
+                                        ? const Color.fromARGB(255, 103, 58, 183)
                                         : Colors.grey[300]!,
                                     width: isSelected ? 2 : 1,
                                   ),
@@ -237,7 +237,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                                         l10n.setLanguage(value);
                                       }
                                     },
-                                    activeColor: Colors.orange,
+                                    activeColor: const Color.fromARGB(255, 103, 58, 183),
                                   ),
                                   onTap: () {
                                     l10n.setLanguage(languageCode);
@@ -284,7 +284,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                                       .read<TutorialProvider>()
                                       .registerAction('toggle_music');
                                 },
-                                activeColor: Colors.orange,
+                                activeColor: const Color.fromARGB(255, 103, 58, 183),
                               ),
                             ),
                           ),
@@ -306,7 +306,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                                   divisions: 10,
                                   label:
                                       '${(widget.gameState.getMusicVolume() * 100).toStringAsFixed(0)}%',
-                                  activeColor: Colors.orange,
+                                  activeColor: const Color.fromARGB(255, 103, 58, 183),
                                   inactiveColor: Colors.grey[300],
                                   onChanged: (value) async {
                                     widget.gameState.setMusicVolume(value);
@@ -540,7 +540,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: Colors.deepOrange),
+          borderSide: const BorderSide(color: Color.fromARGB(255, 103, 58, 183)),
         ),
       ),
     );
@@ -564,7 +564,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: Colors.deepOrange),
+          borderSide: const BorderSide(color: Color.fromARGB(255, 103, 58, 183)),
         ),
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
